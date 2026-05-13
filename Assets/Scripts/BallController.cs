7392002f18cd46a88054d890fe0c4324d32b933e
Elena Ramon <elena.ramon@enti.cat>
2026-05-13 14:14:39 +0200

    level 1 completed

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    [Header("Collision")]
+    public float restitution = 0.8f;
+    private bool isColliding;
+
+    [Header("Hole")]
+    public Transform hole;
+    public float holeRadius = 0.5f;
+    public float maxGoalSpeed = 0.5f;
+
+
     public float mass = 1.0f;
     public float radius = 0.25f;
-
     public float friction = 0.4f;
-
     private float gravity = 9.81f;
 
     private Vector3 velocity;
-
     private float angularVelocity;
 
     // Inputs
@@ -24,12 +31,26 @@ public class BallController : MonoBehaviour
 
     private LineRenderer lineRenderer;
 
+    private string currentSurface = "Grass";
+
+    private bool levelComplete;
+
 
     // Update is called once per frame
     void Update()
     {
+
+        if(levelComplete)
+        {
+            AnimateHoleEntry();
+            return;
+        }
+
         HandleInput();
         SimulatePhysics();
+        CheckWallCollisions();
+        DetectSurface();
+        CheckHole();
     }
 
     void Start()
@@ -75,11 +96,87 @@ public class BallController : MonoBehaviour
         // rotacion
         if (velocity.magnitude > 0.01f)
         {
-            Vector3 rotationAxis =
-                Vector3.Cross(Vector3.up, velocity.normalized);
+            Vector3 rotationAxis = Vector3.Cross(Vector3.up, velocity.normalized);
 
             transform.Rotate( rotationAxis, angularVelocity * Mathf.Rad2Deg * dt, Space.World);
         }
+    }
+
+    void DetectSurface()
+    {
+        RaycastHit hit;
+
+        // Ignore Ball layer
+        int layerMask = ~LayerMask.GetMask("Ball");
+
+        if (Physics.Raycast( transform.position, Vector3.down, out hit, 5f, layerMask))
+        {
+            Debug.Log(hit.collider.name);
+
+            currentSurface = hit.collider.tag;
+
+            switch (currentSurface)
+            {
+                case "Grass":
+                    friction = 0.4f;
+                    break;
+
+                case "Ice":
+                    friction = 0.1f;
+                    break;
+
+                case "Sand":
+                    friction = 0.6f;
+                    break;
+            }
+        }
+    }
+
+    void CheckWallCollisions()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius + 0.05f);
+
+        bool touchingWall = false;
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.CompareTag("Wall"))
+            {
+                touchingWall = true;
+
+                if (!isColliding)
+                {
+                    // punto mas cercano pared
+                    Vector3 closestPoint = hit.ClosestPoint(transform.position);
+
+                    // colision normal
+                    Vector3 normal = (transform.position - closestPoint).normalized;
+
+                    HandleWallCollision(normal);
+
+                    // rebote
+                    transform.position = closestPoint + normal * (radius + 0.01f);
+
+                    isColliding = true;
+                }
+
+                break;
+            }
+        }
+
+        // reseteo colision
+        if (!touchingWall)
+        {
+            isColliding = false;
+        }
+    }
+
+
+    void HandleWallCollision(Vector3 normal)
+    {
+        velocity = Vector3.Reflect(velocity, normal);
+
+        velocity *= restitution;
     }
 
     public void ShootBall(Vector3 direction, float force)
@@ -89,6 +186,12 @@ public class BallController : MonoBehaviour
 
     void HandleInput()
     {
+
+        if (levelComplete)
+        {
+            return;
+        }
+
         // button pressed
         if (Input.GetMouseButtonDown(0))
         {
@@ -103,9 +206,12 @@ public class BallController : MonoBehaviour
         {
             Vector3 currentMousePosition = GetMouseWorldPosition();
 
-            lineRenderer.SetPosition( 0, transform.position);
+            Vector3 direction = (dragStartPosition - currentMousePosition).normalized;
 
-            lineRenderer.SetPosition( 1, currentMousePosition);
+            float lineLength = Mathf.Clamp((dragStartPosition - currentMousePosition).magnitude, 0f, 5f);
+
+            lineRenderer.SetPosition( 0, transform.position);
+            lineRenderer.SetPosition(1, transform.position + direction * lineLength);
         }
 
         // button released
@@ -143,6 +249,39 @@ public class BallController : MonoBehaviour
         return point;
     }
 
+    void CheckHole()
+    {
+        // distancia hacia el agujero
+        float distance = Vector3.Distance(transform.position, hole.position);
+
+        // Bola en agujero
+        if (distance <= holeRadius)
+        {
+            // velocidad de entrada
+            if (velocity.magnitude <= maxGoalSpeed)
+            {
+                levelComplete = true;
+
+                velocity = Vector3.zero;
+            }
+        }
+    }
+
+    void AnimateHoleEntry()
+    {
+        Vector3 targetPosition = new Vector3(hole.position.x, transform.position.y, hole.position.z);
+
+        transform.position = Vector3.Lerp( transform.position, targetPosition, 5f * Time.deltaTime);
+
+        transform.position += Vector3.down * 1.5f * Time.deltaTime;
+
+         if (transform.position.y <= -2f)
+    {
+        gameObject.SetActive(false);
+
+        Debug.Log("LEVEL COMPLETE");
+    }
+    }
 
 }
 
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    // Offset dsd la bola
+    public Vector3 offset = new Vector3(0, 2, -3);
+
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        // posicion
+        Vector3 desiredPosition = target.position + offset;
+
+        // interpolacion
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        transform.position = smoothedPosition;
+        Vector3 lookTarget = target.position + Vector3.forward * 5f;
+
+        transform.LookAt(lookTarget);
+    }
+}
+
